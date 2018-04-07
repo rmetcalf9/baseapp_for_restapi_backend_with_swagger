3c61292bfa7b56c03e1cf26c0464d8016032d038
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-07 13:21:02 +0100

    Adding an example project

---
+++ docs/exampleProject/main.py
@@ -0,0 +1,13 @@
+# Example project that uses baseapp_for_restapi_backend_with_swagger
+
+import baseapp_for_restapi_backend_with_swagger
+
+
+class AppObj(baseapp_for_restapi_backend_with_swagger.AppObj):
+  pass
+
+
+a = AppObj()
+a.ttt()
+
+
