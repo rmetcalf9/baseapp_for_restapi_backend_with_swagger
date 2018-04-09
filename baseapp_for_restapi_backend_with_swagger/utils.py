166cf54035dee31341c4821bb806a1fbb270f2e2
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-09 20:18:29 +0100

    Updating library to not error when timezone returned is tzlocal but this is tzutc

--- baseapp_for_restapi_backend_with_swagger/utils.py
+++ baseapp_for_restapi_backend_with_swagger/utils.py
@@ -1,6 +1,8 @@
 import pytz
 import dateutil.parser
-
+from datetime import datetime
+from dateutil.tz import tzutc
+from dateutil.tz import tzlocal
 
 
 def from_iso8601(when=None, tz=pytz.timezone("UTC")):
@@ -8,7 +10,14 @@ def from_iso8601(when=None, tz=pytz.timezone("UTC")):
   if not _when.tzinfo:
     _when = tz.localize(_when)
   if (str(_when.tzinfo) != 'tzutc()'):
-    raise Exception('Error - Only conversion of utc times from iso8601 is supported')
+    if (str(_when.tzinfo) == 'tzlocal()'):
+      #if the local time is UTC convert it
+      if tzutc().utcoffset(datetime.now()) == tzlocal().utcoffset(datetime.now()):
+        return _when.replace(tzinfo = tzutc())
+      else:
+        raise Exception('Error - Only conversion of utc times from iso8601 is supported (Local time returned but not equlivant offset to UTC)')
+    else:
+      raise Exception('Error - Only conversion of utc times from iso8601 is supported')
   return _when
 
 
