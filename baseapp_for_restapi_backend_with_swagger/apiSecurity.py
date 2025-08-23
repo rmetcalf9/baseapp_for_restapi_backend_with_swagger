35882a592f539cdec116d6e2b5b9eda236446f5b
Robert Metcalf <rmetcalf9@googlemail.com>
2025-08-23 20:06:37 +0100

    Removing debug lines left in

--- baseapp_for_restapi_backend_with_swagger/apiSecurity.py
+++ baseapp_for_restapi_backend_with_swagger/apiSecurity.py
@@ -77,9 +77,6 @@ def apiSecurityCheck(request, tenant, requiredRoleList, headersToSearch, cookies
   return decodedJWTToken
 
 def decodeJWTToken(token, secret, verify):
-  print("token", token)
-  print("secret", secret)
-  print("verify", verify)
   if verify:
     return jwt.decode(token, b64decode(secret), algorithms=['HS256'])
   options = {
